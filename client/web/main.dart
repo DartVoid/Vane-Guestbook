92a9196a11738496e411a58e5f9a230cccaada12
Andreas Ölund <andreas@dartvoid.com>
2014-04-10 09:50:48 +0200

    Added comments and outputs responses to the console

--- client/web/main.dart
+++ client/web/main.dart
@@ -3,25 +3,34 @@ import 'dart:async';
 import 'dart:convert';
 
 void main() {
+  // Form input
   InputElement formName     = querySelector("#form-input-name");
   TextAreaElement formMess  = querySelector("#form-input-message");
   
+  // Form actions
   ButtonElement formSubmit    = querySelector("#form-submit");
   ButtonElement formClearAll  = querySelector("#form-clear-all");
   
+  // Response
   DivElement respWrap = querySelector("#response-wrapper");
   DivElement formResp = querySelector("#response");
   
-  // Fetch list of posts
+  // Load the list of previously added posts
   getPosts().then((response) => addPostsToDOM(formResp, response)); 
   
-  // Submit data
+  // Handle form submit
   formSubmit.onClick.listen((e) {
+    // Prevent default form actions
     e.preventDefault();
+    
+    // Send request to the server
     HttpRequest.request("/posts/add", method: "POST", 
       requestHeaders: {"Content-Type": "application/json"},
       sendData: JSON.encode({"name": formName.value, "message": formMess.value}))
     .then((response) {
+      print(response);
+      
+      // Reload list of posts
       getPosts().then((response) => addPostsToDOM(formResp, response));
     }).catchError((e) {
       print("Unable to add post");
@@ -30,9 +39,13 @@ void main() {
   
   // Remove all posts
   formClearAll.onClick.listen((e) {
+    // Prevent default form actions
     e.preventDefault();
+    
+    // Clear list of posts
     formResp.children.clear();
     
+    // Delete posts from the database
     deletePosts().then((response) { 
       print(response);
       getPosts().then((response) => addPostsToDOM(formResp, response));
@@ -40,12 +53,14 @@ void main() {
   });
 }
 
+// Add a post to the dom
 void addPostsToDOM(DivElement posts, String response) {
   if(response != "") {
     List responseList = JSON.decode(response);
     posts.children.clear();
     responseList.forEach((Map post) {
       
+      // Create a new dom element for each post
       DivElement postDiv = new DivElement()
         ..classes = ["content-offset", "well"]
         ..children.addAll(
@@ -53,20 +68,22 @@ void addPostsToDOM(DivElement posts, String response) {
           new ParagraphElement()
             ..classes.add("text-bold")
             ..text = "${post["name"]}",
-            
           new ParagraphElement()
             ..text = "${post["message"]}"
         ]
       );
       
+      // Append the new element to parent div
       posts.append(postDiv);
-      
     });
   }
 }
 
+// Get all posts
 Future<String> getPosts() {
   var c = new Completer();
+  
+  // Send request to the server
   HttpRequest.request("/posts/list", method: "GET").then((response) {
     c.complete(response.responseText); 
   }).catchError((e) {
@@ -77,8 +94,11 @@ Future<String> getPosts() {
   return c.future;
 }
 
+// Delete all posts
 Future deletePosts() {
   var c = new Completer();
+  
+  // Send request to the server
   HttpRequest.request("/posts/delete", method: "DELETE").then((response) {
     c.complete(response.responseText); 
   }).catchError((e) {
